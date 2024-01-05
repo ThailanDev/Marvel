d4056a332adb813e0f0688bf3ffef2180f60cb8d
Thailan Godoy <thailanrg@gmail.com>
2024-01-05 09:45:56 -0300

    configurando okhttp

--- core-android/src/main/java/com/example/core_android/network/api/RetrofitConfig.kt
+++ core-android/src/main/java/com/example/core_android/network/api/RetrofitConfig.kt
@@ -3,6 +3,7 @@ package com.example.core_android.network.api
 import com.example.core_android.network.api.constants.Constants
 import com.example.core_android.network.api.service.ApiService
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,7 +15,16 @@ val networkModule = module {
 }
 
 fun provideOkHttpClient(): OkHttpClient {
-    return OkHttpClient()
+    val loggin = HttpLoggingInterceptor()
+    loggin.level = HttpLoggingInterceptor.Level.BODY
+
+    return OkHttpClient().newBuilder()
+        .addInterceptor{ chain ->
+            val currentTimestamp = System.currentTimeMillis()
+            val newUrl = chain.request().url()
+                .newBuilder()
+                .addQueryParameter()
+        }
 }
 fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
     return Retrofit.Builder()
