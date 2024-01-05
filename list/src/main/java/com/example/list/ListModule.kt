dbf655e7d550033b560c6530fa65601d02945ed3
Thailan Godoy <thailanrg@gmail.com>
2024-01-04 22:34:20 -0300

    adicionando dependencia para delegação de viewModel e resolvendo crashs

--- list/src/main/java/com/example/list/ListModule.kt
+++ list/src/main/java/com/example/list/ListModule.kt
@@ -5,10 +5,12 @@ import com.example.list.repository.ListDataSourcerImp
 import com.example.list.repository.ListRepository
 import com.example.list.repository.ListRepositoryImpl
 import com.example.list.usecase.ListUseCase
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val ListScreenModule = module {
     single <ListDataSourcerImp> { ListDataSource(get()) }
     single <ListRepositoryImpl> { ListRepository(get()) }
     single { ListUseCase(get()) }
+    viewModel { ListViewModel(get()) }
 }
--- list/src/main/java/com/example/list/ListViewModel.kt
+++ list/src/main/java/com/example/list/ListViewModel.kt
@@ -3,13 +3,14 @@ package com.example.list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.list.usecase.ListUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ListViewModel(
     private val useCase: ListUseCase
 ) : ViewModel() {
     fun getList(){
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             useCase.getList()
         }
     }
