a6b9da1797b29e43093de70f518dfd305b9a7a3e
Thailan Godoy <thailanrg@gmail.com>
2024-01-06 11:04:26 -0300

    ajustando metodos de sucesso e error da tela de lista

--- list/src/main/java/com/example/list/ListViewModel.kt
+++ list/src/main/java/com/example/list/ListViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.list
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -20,21 +21,22 @@ internal class ListViewModel(
     }
 
     private fun getList() {
+        state.postValue(state.value?.copy(isLoading = true))
         viewModelScope.launch(Dispatchers.IO) {
             useCase().fold(::isError, ::isSuccess)
         }
     }
 
     private fun isError(e: Exception) {
-        state.value?.copy(isError = true)
+        state.postValue(state.value?.copy(isError = true, isLoading = false))
     }
 
     private fun isSuccess(data: CharacterDataWrapper?) {
-        state.postValue(ListState(data = data))
+        state.postValue(state.value?.copy(data = data, isLoading = false))
     }
 
     internal data class ListState(
-        val data: CharacterDataWrapper?,
+        var data: CharacterDataWrapper?,
         val isLoading: Boolean = false,
         val isError: Boolean = false
     )
